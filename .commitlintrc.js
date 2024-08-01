89040106a3d2da3dde114864a559f63c7901ed02
guang <531311582@qq.com>
2024-08-02 00:46:48 +0800

    feat: add commitlint

---
+++ .commitlintrc.js
@@ -0,0 +1,6 @@
+/**
+ * @author        guang <531311582@qq.com>
+ * @date          2024-08-02 00:39:10
+ * Copyright © YourCompanyName All rights reserved
+ */
+module.exports = { extends: ["@commitlint/config-conventional"] };
